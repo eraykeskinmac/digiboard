56789bda701bfd9a39a50db980a2dcf356c1f832
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-16 02:20:27 +0300

    docs: add RoomContextProvider for sharing board position

---
+++ modules/room/context/Room.context.tsx
@@ -0,0 +1,18 @@
+import { MotionValue, useMotionValue } from 'framer-motion';
+import { ReactChild, createContext } from 'react';
+
+export const roomContext = createContext<{
+  x: MotionValue<number>;
+  y: MotionValue<number>;
+}>(null!);
+
+const RoomContextProvider = ({ children }: { children: ReactChild }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  return (
+    <roomContext.Provider value={{ x, y }}>{children}</roomContext.Provider>
+  );
+};
+
+export default RoomContextProvider;
