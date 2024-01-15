7b88da16098d3b9abadc314d50ab921d4913b8a2
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-16 02:20:26 +0300

    chore: update dependencies and package versions

--- common/types/global.d.ts
+++ common/types/global.d.ts
@@ -1,14 +1,17 @@
 export declare global {
   interface CtxOptions {
-    lineWidth: number
-    lineColor: string
+    lineWidth: number;
+    lineColor: string;
   }
 
   interface ServerToClientEvents {
-    socket_draw: (newMoves: [number, number][], options: CtxOptions) => void
+    socket_draw: (newMoves: [number, number][], options: CtxOptions) => void;
+    mouse_moved: (x: number, y: number, socketId: string) => void;
+    users_in_room: (socketIds: string[]) => void;
   }
 
   interface ClientToServerEvents {
-    draw: (moves: [number, number][], options: CtxOptions) => void
+    draw: (moves: [number, number][], options: CtxOptions) => void;
+    mouse_move: (x: number, y: number) => void;
   }
 }
---
+++ modules/room/components/MouseRenderer.tsx
@@ -0,0 +1,27 @@
+import { socket } from '@/common/lib/socket';
+import { useEffect, useState } from 'react';
+import { SocketMouse } from './SocketMouse';
+
+export const MouseRenderer = () => {
+  const [mouses, setMouses] = useState<string[]>([]);
+
+  console.log(mouses);
+
+  useEffect(() => {
+    socket.on('users_in_room', (socketIds) => {
+      const allUsers = socketIds.filter((socketId) => socketId !== socket.id);
+      setMouses(allUsers);
+    });
+    return () => {
+      socket.off('users_in_room');
+    };
+  }, []);
+
+  return (
+    <>
+      {mouses.map((socketId) => {
+        return <SocketMouse socketId={socketId} key={socketId} />;
+      })}
+    </>
+  );
+};
