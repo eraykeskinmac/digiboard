9cd5e3c30ec6e9eeb7c993de1a193042e097ffe3
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-23 23:16:43 +0300

    feat: add drawing and undo functionality

--- modules/room/hooks/Canvas.hooks.ts
+++ modules/room/hooks/Canvas.hooks.ts
@@ -1,18 +1,27 @@
+import { useBoardPosition } from './useBoardPosition';
 import { socket } from '@/common/lib/socket';
 import { useOptions } from '@/common/recoil/options';
-import { useEffect, useState } from 'react';
+import { KeyboardEventHandler, useCallback, useEffect, useState } from 'react';
+import { drawOnUndo } from '../helpers/Canvas.helpers';
+import usersAtom, { useUsers } from '@/common/recoil/users';
+import { getPos } from '@/common/lib/getPos';
+import { useSetRecoilState } from 'recoil';
 
+const savedMoves: [number, number][][] = [];
 let moves: [number, number][] = [];
 
 export const useDraw = (
   ctx: CanvasRenderingContext2D | undefined,
   blocked: boolean,
-  movedX: number,
-  movedY: number,
   handleEnd: () => void
 ) => {
+  const users = useUsers();
   const options = useOptions();
   const [drawing, setDrawing] = useState(false);
+
+  const boardPosition = useBoardPosition();
+  const movedX = boardPosition.x;
+  const movedY = boardPosition.y;
 
   useEffect(() => {
     if (ctx) {
@@ -23,39 +32,116 @@ export const useDraw = (
     }
   });
 
+  const handleUndo = useCallback(() => {
+    if (ctx) {
+      savedMoves.pop();
+      socket.emit('undo');
+
+      drawOnUndo(ctx, savedMoves, users);
+
+      handleEnd();
+    }
+  }, [ctx, handleEnd, users]);
+
+  useEffect(() => {
+    const handleUndoKeyboard = (e: KeyboardEvent) => {
+      if (e.key === 'z' && e.ctrlKey) {
+        handleUndo();
+      }
+    };
+
+    document.addEventListener('keydown', handleUndoKeyboard);
+
+    return () => {
+      document.removeEventListener('keydown', handleUndoKeyboard);
+    };
+  }, [handleUndo]);
+
   const handleStartDrawing = (x: number, y: number) => {
     if (!ctx || blocked) return;
 
-    moves = [[x + movedX, y + movedY]];
     setDrawing(true);
 
     ctx.beginPath();
-    ctx.lineTo(x + movedX, y + movedY);
+    ctx.lineTo(getPos(x, movedX), getPos(y, movedY));
     ctx.stroke();
   };
 
   const handleEndDrawing = () => {
     if (!ctx || blocked) return;
 
-    socket.emit('draw', moves, options);
     setDrawing(false);
     ctx.closePath();
-    handleEnd();
+    savedMoves.push(moves);
+    socket.emit('draw', moves, options);
+
+    moves = [];
   };
 
   const handleDraw = (x: number, y: number) => {
     if (!ctx || !drawing || blocked) {
       return;
     }
-    moves.push([x + movedX, y + movedY]);
-    ctx.lineTo(x + movedX, y + movedY);
+    ctx.lineTo(getPos(x, movedX), getPos(y, movedY));
     ctx.stroke();
+
+    moves.push([getPos(x, movedX), getPos(y, movedY)]);
   };
 
   return {
     handleDraw,
     handleEndDrawing,
     handleStartDrawing,
+    handleUndo,
     drawing,
   };
+};
+
+export const useSocketDraw = (
+  ctx: CanvasRenderingContext2D | undefined,
+  handleEnd: () => void
+) => {
+  const setUsers = useSetRecoilState(usersAtom);
+
+  useEffect(() => {
+    socket.on('user_draw', (newMoves, options, userId) => {
+      if (ctx) {
+        ctx.lineWidth = options.lineWidth;
+        ctx.strokeStyle = options.lineColor;
+
+        ctx.beginPath();
+        newMoves.forEach(([x, y]) => {
+          ctx.lineTo(x, y);
+        });
+        ctx.stroke();
+        ctx.closePath();
+
+        handleEnd();
+
+        setUsers((prevUsers) => {
+          const newUsers = { ...prevUsers };
+
+          newUsers[userId] = [...newUsers[userId], newMoves];
+          return newUsers;
+        });
+      }
+    });
+    socket.on('user_undo', (userId) => {
+      setUsers((prevUsers) => {
+        const newUsers = { ...prevUsers };
+        newUsers[userId] = newUsers[userId].slice(0, -1);
+
+        if (ctx) {
+          drawOnUndo(ctx, savedMoves, newUsers);
+          handleEnd();
+        }
+
+        return newUsers;
+      });
+    });
+    return () => {
+      socket.off('user_draw');
+      socket.off('user_undo');
+    };
+  });
 };
