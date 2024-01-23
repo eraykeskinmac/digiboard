5feb559f2288190555d56a56f7f73ace92c14e65
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-23 23:16:43 +0300

    feat: handle socket events and user disconnection

--- modules/room/context/Room.context.tsx
+++ modules/room/context/Room.context.tsx
@@ -1,5 +1,8 @@
+import { socket } from '@/common/lib/socket';
+import usersAtom, { useUserIds } from '@/common/recoil/users';
 import { MotionValue, useMotionValue } from 'framer-motion';
-import { ReactChild, createContext } from 'react';
+import { ReactChild, createContext, useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 export const roomContext = createContext<{
   x: MotionValue<number>;
@@ -7,8 +10,32 @@ export const roomContext = createContext<{
 }>(null!);
 
 const RoomContextProvider = ({ children }: { children: ReactChild }) => {
+  const setUsers = useSetRecoilState(usersAtom);
+  const userIds = useUserIds();
   const x = useMotionValue(0);
   const y = useMotionValue(0);
+
+  useEffect(() => {
+    socket.on('users_in_room', (newUsers) => {
+      newUsers.forEach((user) => {
+        if (!userIds.includes(user) && user !== socket.id) {
+          setUsers((prevUsers) => ({ ...prevUsers, [user]: [] }));
+        }
+      });
+    });
+
+    socket.on('user_disconnected', (userId) => {
+      setUsers((prevUsers) => {
+        const newUsers = { ...prevUsers };
+        delete newUsers[userId];
+        return newUsers;
+      });
+    });
+    return () => {
+      socket.off('users_in_room');
+      socket.off('user_disconnected');
+    };
+  }, [setUsers, userIds]);
 
   return (
     <roomContext.Provider value={{ x, y }}>{children}</roomContext.Provider>
