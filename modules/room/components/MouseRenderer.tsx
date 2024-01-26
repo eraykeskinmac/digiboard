821bb34909c8ce513e011da3d1e3bb31eeea08f1
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-26 17:34:30 +0300

    refactor: update MouseRenderer to use useUserIds hook and remove local state

--- modules/room/components/MouseRenderer.tsx
+++ modules/room/components/MouseRenderer.tsx
@@ -1,26 +1,13 @@
-import { socket } from '@/common/lib/socket';
-import { useEffect, useState } from 'react';
 import { SocketMouse } from './SocketMouse';
+import { useUserIds } from '@/common/recoil/users';
 
 export const MouseRenderer = () => {
-  const [mouses, setMouses] = useState<string[]>([]);
-
-  console.log(mouses);
-
-  useEffect(() => {
-    socket.on('users_in_room', (socketIds) => {
-      const allUsers = socketIds.filter((socketId) => socketId !== socket.id);
-      setMouses(allUsers);
-    });
-    return () => {
-      socket.off('users_in_room');
-    };
-  }, []);
+  const userIds = useUserIds();
 
   return (
     <>
-      {mouses.map((socketId) => {
-        return <SocketMouse socketId={socketId} key={socketId} />;
+      {userIds.map((userId) => {
+        return <SocketMouse userId={userId} key={userId} />;
       })}
     </>
   );
